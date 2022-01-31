5c8872d7312add78d6fba9f81a5408bea5c8eb77
propro1027 <for.pro.business.address@gmail.com>
2022-01-31 21:48:14 +0900

    timezone cofig app

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,9 @@ module New
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+    config.time_zone = 'Asia/Tokyo'
+
+
 
     # Configuration for the application, engines, and railties goes here.
     #
