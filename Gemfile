19d8550045b7fd55c0fa48f44d246e69693fc5bb
propro1027 <for.pro.business.address@gmail.com>
2022-02-05 16:32:04 +0900

    top

--- Gemfile
+++ Gemfile
@@ -1,6 +1,8 @@
 source "https://rubygems.org"
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
+gem 'bootstrap-sass'
+
 # ruby "2.7.5"
 ruby "2.7.0"
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get '/signup', to: 'users#new'
+  root 'home#top'
+  # get 'home/top'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
