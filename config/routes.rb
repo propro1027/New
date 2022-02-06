afee75e9755f4881908ea10a00c7bd659442b0f5
propro1027 <for.pro.business.address@gmail.com>
2022-02-06 15:20:35 +0900

    table

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
-  get '/signup', to: 'users#new'
   root 'home#top'
-  # get 'home/top'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get '/signup', to: 'users#new'
+  resources :users
 end
