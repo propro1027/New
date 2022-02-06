7c11cc9704e640335e56dabfa2f230590a1511e4
propro1027 <for.pro.business.address@gmail.com>
2022-02-06 21:34:43 +0900

    data save ok but flash messages dont appear

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,10 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+		flash.now[:success] = "新規登録が完了しました。"
+		redirect_to @user
     else
+	  flash.now[:danger] = "新規登録に失敗しました。"
       render :new
     end
   end
